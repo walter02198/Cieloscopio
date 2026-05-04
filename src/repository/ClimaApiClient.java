0833e572572445de47ff1ae13f66eb51e8308e7b
walter02198 <waltervalverdesalinas@gmail.com>
2026-05-04 10:43:42 -0400

    Implementacio: agregar una capa de infraestructura para obtener datos meteorológicos de la API de OpenWeather y deserializar JSON a DTOs.(DTOs).

---
+++ src/repository/ClimaApiClient.java
@@ -0,0 +1,41 @@
+package repository;
+
+import com.google.gson.Gson;
+import dto.ClimaOpenWeatherMap;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ClimaApiClient implements IClimaRepository {
+
+    private final String API_KEY = "c0cbd1c787c4b8f6e495732a58a9e29a";
+    private final Gson gson = new Gson();
+
+    @Override
+    public ClimaOpenWeatherMap consultarClima(String ciudad) {
+        String url = "https://api.openweathermap.org/data/2.5/weather?q="
+                + ciudad + "&appid=" + API_KEY;
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .build();
+
+            // Enviamos la petición
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            // Aquí es donde GSON hace su trabajo
+            // Convierte el String JSON directamente en tu Record DTO
+            String json = response.body();
+            return gson.fromJson(json, ClimaOpenWeatherMap.class);
+
+        } catch (Exception e) {
+            // Lógica simple de manejo de errores
+            throw new RuntimeException("Error al conectar con la API: " + e.getMessage());
+        }
+    }
+}
+
