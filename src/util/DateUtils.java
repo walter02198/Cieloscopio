310974dccdd84f5986be3b9fd4f1a934f8b3aa5a
walter02198 <waltervalverdesalinas@gmail.com>
2026-05-04 16:58:58 -0400

    Se dividio la logica en dos metodos una para fecha y el otro para la hora actual al pedir los datos de la ciudad

--- src/util/DateUtils.java
+++ src/util/DateUtils.java
@@ -1,14 +1,22 @@
 package util;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateUtils {
 
-    public static String obtenerFechaHoraActual() {
-        LocalDateTime ahora = LocalDateTime.now();
-        // Definimos un formato amigable: "dd/MM/yyyy HH:mm"
-        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+    public static String obtenerFechaActual() {
+        LocalDate ahora = LocalDate.now();
+        // Definimos un formato amigable: "dd/MM/yyyy"
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return ahora.format(formato);
+    }
+    public static String obtenerHoraActual() {
+        LocalTime ahora = LocalTime.now();
+        // Definimos un formato amigable: "HH:mm"
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("HH:mm");
         return ahora.format(formato);
     }
 }
